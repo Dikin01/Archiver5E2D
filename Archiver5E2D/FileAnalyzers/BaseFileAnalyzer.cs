2c47948269aa3b3221c2d53312430836e71b5d5e
Dmitrii <kondim007@mail.ru>
2022-10-07 19:16:03 +0300

    feat: add file analyzers

---
+++ Archiver5E2D/FileAnalyzers/BaseFileAnalyzer.cs
@@ -0,0 +1,50 @@
+namespace Archiver5E2D.FileAnalyzers;
+
+public abstract class BaseFileAnalyzer<T> where T : notnull
+{
+    protected readonly byte[] _fileBytes;
+
+    public abstract long Length { get; }
+
+    public BaseFileAnalyzer(string path)
+    {
+        if (!File.Exists(path))
+            throw new FileNotFoundException("File not found", Path.GetFileName(path));
+
+        _fileBytes = File.ReadAllBytes(path);
+    }
+
+    public abstract Dictionary<T, long> GetCountOccurrences();
+
+    public Dictionary<T, double> GetProbabilities()
+    {
+        var occurrences = GetCountOccurrences();
+
+        return occurrences
+            .ToDictionary(occurrence => occurrence.Key,
+                occurrence => (double) occurrence.Value / Length);
+    }
+
+    public Dictionary<T, double> GetInfoAmountInSymbol()
+    {
+        var probabilities = GetProbabilities();
+        
+        return probabilities
+            .ToDictionary(probability => probability.Key,
+                probability => - Math.Log2(probability.Value / Length));
+    }
+    
+    public (double bits, double bytes) GetInfoAmount()
+    {
+        var infoAmountInSymbol = GetInfoAmountInSymbol();
+        var probabilities = GetProbabilities();
+
+        var bits = infoAmountInSymbol
+            .Where(item => item.Value != 0)
+            .Sum(item => item.Value * probabilities[item.Key]);
+
+        var bytes = Math.Ceiling(bits);
+
+        return (bits, bytes);
+    }
+}
