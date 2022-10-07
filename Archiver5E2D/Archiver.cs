45f62b782fdbdd0bbfa870f614463b35775627d7
Dmitrii <kondim007@mail.ru>
2022-10-07 18:57:26 +0300

    fix: merge conflict

--- Archiver5E2D/Archiver.cs
+++ Archiver5E2D/Archiver.cs
@@ -30,6 +30,8 @@ public static class Archiver
         var archivedFile = File.FromExistingFile(path);
         var compressor = CompressorProvider.ProvideForCompressedFile(archivedFile);
         var combinedFile = compressor.Decompress(archivedFile);
+
+        return EntitiesConverter.SplitIntoEntities(combinedFile);
     }
 
     private static File Archive(IEntity entity)
